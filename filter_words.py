1584f5e8e8417f41064e32f796c99c416771be2b
Oriado <dasharom5555@gmail.com>
2025-09-21 22:27:45 +0300

    Додавання файлу filter_words.py

---
+++ filter_words.py
@@ -0,0 +1,12 @@
+def filter_words_by_length(words, min_length=3):
+    
+    filter = []
+    
+    for word in words:
+        if len(word) > min_length:
+            filter.append(word)
+    return filter            
+
+words = ["flowers", "kitchen", "dog", "mouse"]
+result = filter_words_by_length(words, 4)
+print(result)      
